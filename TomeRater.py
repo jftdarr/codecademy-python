cb4bef2ece997dbe68bb366ea8678b0554c1e910
jftdarr <jft.darr@gmail.com>
2018-10-14 23:23:36 -0400

    Completed Project

--- TomeRater.py
+++ TomeRater.py
@@ -1,3 +1,4 @@
+import re
 class User(object):
     def __init__(self, name, email):
         self.name = name
@@ -24,9 +25,15 @@ class User(object):
         else:
             return False
 
+    def read_book(self, book, rating = None):
+        self.books[book] = rating
 
-class Book:
-    def __init__(self,title,isbn):
+    def get_average_rating(self):
+        return sum([rating for rating in self.books.values() if rating is not None]) / len(self.books)
+
+
+class Book(object):
+    def __init__(self, title, isbn):
         self.title = title
         self.isbn = isbn
         self.ratings = []
@@ -38,14 +45,21 @@ class Book:
         return self.isbn
 
     def set_isbn(self, new_isbn):
-        self.isbn = new_isbn
-        return f"{self.name} ISBN has been updated to {self.isbn}"
-
-    def add_rating(self,rating):
-        if 0 <= rating < 5:
-            self.ratings.append(rating)
+        if new_isbn == self.isbn:
+            return f"{self.isbn} is already in the catalog"
         else:
-            print("Invalid Rating")
+            self.isbn = new_isbn
+            return f"{self.title} ISBN has been updated to {self.isbn}"
+
+    def add_rating(self, rating):
+        try:
+            if 0 <= rating < 5:
+                self.ratings.append(rating)
+            else:
+                return "Invalid Rating"
+
+        except TypeError:
+            "Invalid Type"
 
     def __eq__(self, other_book):
         if self.title == other_book.title:
@@ -56,14 +70,100 @@ class Book:
         else:
             return False
 
+    def get_average_rating(self):
+        return round(sum(self.ratings)/len(self.ratings),1)
+
+    def __hash__(self):
+        return hash((self.title, self.isbn))
+
 class Fiction(Book):
     def __init__(self, title, author, isbn):
         super().__init__(title, isbn)
         self.author = author
-        
+
     def get_author(self):
         return self.author
-    
+
     def __repr__(self):
         return f"{self.title} by {self.author}"
 
+
+class Non_Fiction(Book):
+    def __init__(self, title, subject, level, isbn):
+        super().__init__(title, isbn)
+        self.subject = subject
+        self.level = level
+
+    def get_subject(self):
+        return self.subject
+
+    def get_level(self):
+        return self.level
+
+    def __repr(self):
+        return f"{self.title}, a {self.level} manual on {self.subject}"
+
+
+class TomeRater(object):
+    def __init__(self):
+        self.users = {}
+        self.books = {}
+
+    @staticmethod
+    def create_book(title, isbn):
+        return Book(title,isbn)
+
+    @staticmethod
+    def create_novel(title, author, isbn):
+        return Fiction(title, author, isbn)
+
+    @staticmethod
+    def create_non_fiction(title, subject, level, isbn):
+        return Non_Fiction(title, subject, level, isbn)
+
+    def add_book_to_user(self, book, email, rating = None):
+        user = self.users.get(email, f"No user with email {email}!")
+
+        if user:
+            user.read_book(book, rating)
+            book.add_rating(rating)
+
+            self.books[book] = self.books.get(book, 0) + 1
+
+    def add_user(self, name, email, user_books = None):
+        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+
+        if match is None:
+            print('Bad Syntax')
+            raise ValueError('Bad Syntax')
+        else:
+            if email not in self.users:
+                self.users[email] = User(name, email)
+                if user_books is not None:
+                    for book in user_books:
+                        self.add_book_to_user(book, email)
+            else:
+                print(f"user {email} already exists...")
+
+    # Analysis methods
+    def print_catalog(self):
+        for key in self.books.keys():
+            print(key)
+
+    def print_users(self):
+        for value in self.users.values():
+            print(value)
+
+    def get_most_read_book(self):
+        return max(self.books, key=self.books.get)
+
+
+    def highest_rated_book(self):
+        highest_rated = max(rating.get_average_rating() for rating in self.books.keys())
+
+        return str([book for book in self.books.keys() if book.get_average_rating() == highest_rated]).strip('[]')
+
+    def most_positive_user(self):
+        highest_rated = max(rating.get_average_rating() for rating in self.users.values())
+
+        return str([user for user in self.users.values() if user.get_average_rating() == highest_rated]).strip('[]')
